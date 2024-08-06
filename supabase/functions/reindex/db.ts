ff7c161ea8429d529ff436ba3dd4059f3bb90b46
Kristofer <kristofer@fmckl.se>
2024-08-06 18:13:57 +0200

    Use SUPABASE_ env

--- supabase/functions/reindex/db.ts
+++ supabase/functions/reindex/db.ts
@@ -4,6 +4,6 @@ import { Database } from "./database.types.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
 export const supabase = createClient<Database>(
-  Deno.env.get("DB_URL")!,
-  Deno.env.get("DB_SERVICE_ROLE_KEY")!,
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );

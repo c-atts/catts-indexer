870b535a1bbc25fcb08964a0228ed72a501ff8b7
Kristofer <kristofer@fmckl.se>
2024-08-07 20:58:56 +0200

    Add support for updating recipes

--- supabase/functions/reindex/recipe.ts
+++ supabase/functions/reindex/recipe.ts
@@ -1,10 +1,11 @@
 import { ChangeLogItem, recipeSchema } from "./rpc.types.ts";
+import { applyPatch, Patch } from "npm:rfc6902";
 
 import { supabase } from "./db.ts";
 
 export async function createRecipe(item: ChangeLogItem) {
   const recipe = recipeSchema.parse(JSON.parse(item.patch));
-  const { selectError } = await supabase
+  const { error: selectError } = await supabase
     .from("recipe")
     .insert([
       recipe,
@@ -12,5 +13,42 @@ export async function createRecipe(item: ChangeLogItem) {
 
   if (selectError) {
     throw selectError;
+  }
+}
+
+export async function updateRecipe(item: ChangeLogItem) {
+  const patch: Patch = JSON.parse(item.patch);
+
+  const { data, error: selectError } = await supabase
+    .from("recipe")
+    .select("*")
+    .eq("id", item.id)
+    .single();
+
+  if (selectError) {
+    throw selectError;
+  }
+
+  if (!data) {
+    throw new Error("Recipe not found");
+  }
+
+  applyPatch(data, patch);
+
+  // Convert created to ISO string
+  if ("created" in data && typeof data.created === "number") {
+    data.created = new Date(data.created * 1000).toISOString();
+  }
+
+  // Update recipe
+  const { error: updateError } = await supabase
+    .from("recipe")
+    .update({
+      ...data,
+    })
+    .eq("id", item.id);
+
+  if (updateError) {
+    throw updateError;
   }
 }
--- supabase/functions/reindex/run.ts
+++ supabase/functions/reindex/run.ts
@@ -1,24 +1,24 @@
 import { ChangeLogItem, runSchema } from "./rpc.types.ts";
+import { applyPatch, Patch } from "npm:rfc6902";
 
-import { applyPatch } from "npm:rfc6902";
 import { supabase } from "./db.ts";
 
 export async function createRun(item: ChangeLogItem) {
   const run = runSchema.parse(JSON.parse(item.patch));
 
-  const { error } = await supabase
+  const { error: selectError } = await supabase
     .from("run")
     .insert([
       run,
     ]);
 
-  if (error) {
-    throw error;
+  if (selectError) {
+    throw selectError;
   }
 }
 
 export async function updateRun(item: ChangeLogItem) {
-  const patch = JSON.parse(item.patch);
+  const patch: Patch = JSON.parse(item.patch);
 
   const { data, error: selectError } = await supabase
     .from("run")
@@ -35,6 +35,11 @@ export async function updateRun(item: ChangeLogItem) {
   }
 
   applyPatch(data, patch);
+
+  // Convert created to ISO string
+  if ("created" in data && typeof data.created === "number") {
+    data.created = new Date(data.created * 1000).toISOString();
+  }
 
   // Update run
   const { error: updateError } = await supabase
